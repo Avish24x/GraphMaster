cca90643cdd5bc771f242b6fa6a9fb348805437b
Milesh Taukoorah <mileshtaukoorah@gmail.com>
2023-09-22 00:03:03 +0400

    added grids and added grid display for medium screen large and small screen

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Box, CssBaseline } from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Navbar from "@/scenes/navbar";
+import Dashboard from "@/scenes/dashboard"
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
@@ -18,7 +19,7 @@ function App() {
           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
             <Navbar />
             <Routes>
-              <Route path="/" element={<div>dashboard page</div>} />
+              <Route path="/" element={<Dashboard/>} />
               <Route path="/predictions" element={<div>prediction page</div>} />
             </Routes>
           </Box>
